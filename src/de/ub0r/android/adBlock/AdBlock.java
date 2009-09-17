35b1a79966db4872503821a6e0717f713ff691ba
Felix Bechstein <f@ub0r.de>
2009-09-17 18:38:23 +0200

    init

---
+++ src/de/ub0r/android/adBlock/AdBlock.java
@@ -0,0 +1,13 @@
+package de.ub0r.android.adBlock;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AdBlock extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
